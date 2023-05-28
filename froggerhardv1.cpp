5bfe105e9be26aafe4135ef618852850e403899a
Justin T. Alexander <102481480+MrStarman18@users.noreply.github.com>
2023-05-28 15:04:20 -0500

    Working on Froggerhard
    Exceeded time limit, likely for the obvious reason.
    Separated first attempt into v1 file and starting on a cache.

--- froggerhardv1.cpp
+++ froggerhardv1.cpp
@@ -1,12 +1,11 @@
 //Kattis Problem 1-D Frogger Hard
+//This version exceeded the time limit.
 #include <iostream>
 #include <vector>
 #include <set>
 #include <algorithm>
 using namespace std;
 
-vector<vector<int>> winPairs; //caching, for speed
-vector<vector<int>> losePairs; //caching, for speed
 vector<int> board;
 int numSpaces; 
 int total = 0;
@@ -25,16 +24,13 @@ void frogger(int start, int magic)
 		}
 		int move = frog + board[frog];
 		
-		if(move < 0 || move > numSpaces || visited.count(move)) 
-		{
-			losePairs[start].push_back(magic);	//this (start,magic is a losing pair)
-			return;
-		}
+		if(move < 0 || move > numSpaces) return;
+		else if (visited.count(move)) return;
+		//else if( std::find(visited.begin(), visited.end, move)) return;
 		else
 		{
 			frog = move;
 			visited.insert(move);
-			winPairs[start].push_back(magic);	//this (start,magic is a winning pair)
 			//cout << "Inserting: " << move << endl;
 		}
 	}
@@ -48,9 +44,6 @@ int main() {
 		int middleMan;
 		cin >> middleMan;
 		board.push_back(middleMan);
-		vector<int> middleVect;
-		winPairs.push_back(middleVect);
-		losePairs.push_back(middleVect);
 	}
 	
 	//Traverse the board
@@ -59,11 +52,7 @@ int main() {
 		for(int j = 0; j < numSpaces; j++)	//use each possible magic number for that start
 		{
 			//if (j == i) continue;	//i is a position, j is a number
-			if(find(winPairs[i].begin(), winPairs[i].end(), j) == winPairs[i].end())
-				frogger(i, board[j]);
-			else if(find(losePairs[i].begin(), losePairs[i].end(), j) == losePairs[i].end())
-				continue;
-			else total++;
+			frogger(i, board[j]);
 		}
 	}
 
